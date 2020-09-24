84bb6c1034ff3ceafae9a1e1f374cd06f0a66bdb
thetrung <deulamco@gmail.com>
2020-09-24 20:12:12 +0700

    working auth with anonymous & full REST API access to firebase

--- firebase/database.lua
+++ firebase/database.lua
@@ -1,43 +1,81 @@
---
--- Test -> PUT
---
-local function test_get()
-	local url = 'https://bool-8ba6d.firebaseio.com/users/000.json?auth=' .. AUTH_TOKEN
-	print(url)
-	http.request(url, 'GET', function(self,_, response)
-		print(response.response)
-	end, 
-	nil, nil, nil)
+Database = {}
+
+function Database:new(project_id)
+	--
+	-- saving info 
+	--
+	local obj = {}
+	local token = nil
+	local project_id = project_id
+	--
+	-- Sample curl:
+	--
+	-- curl 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=API_KEY' \
+	-- -H 'Content-Type: application/json' --data-binary '{"returnSecureToken":true}'
+
+	function obj:request_token(api_key, callback)
+		local token_url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' .. api_key
+		local headers = {
+			['Content-Type'] = 'application/json'
+		}
+		local post_data = { ["returnSecureToken"] = true }
+		local req_token = http.request(token_url, 'POST', function(self,_,res)
+			print("database -- got res: " .. res.response)
+			token = cjson.decode(res.response)["idToken"]
+			print("database -- token=" .. token)
+			callback()
+		end,
+	    headers, cjson.encode(post_data), nil)
+	end
+	
+	local function base_request(project_id, path, callback, data, method)
+		local headers = nil 
+		local url = 'https://' .. project_id .. '.firebaseio.com' .. path ..  '.json' .. '?auth=' .. token
+		print(url)
+		
+		http.request(url, method, function(self,_, response)
+			-- print(response.response)
+			callback(response)
+		end, 
+		headers, cjson.encode(data), option)
+		-- http.request(url, method, callback, headers, post_data, option)
+	end
+	--
+	-- OBJ -> GET
+	--
+	function obj:get(path, callback)
+		base_request(project_id, path, callback, nil, 'GET')
+	end
+	--
+	-- OBJ -> PUT
+	--
+	function obj:put(path, callback, data)
+		base_request(project_id, path, callback, data, 'PUT')
+	end
+	--
+	-- OBJ -> POST
+	--
+	function obj:post(path, callback, post_data)
+		base_request(project_id, path, callback, post_data, 'POST')
+	end
+	--
+	-- OBJ -> PATCH
+	--
+	function obj:patch(path, callback, post_data)
+		base_request(project_id, path, callback, post_data, 'PATCH')
+	end
+	--
+	-- OBJ -> DELETE
+	--
+	function obj:delete(path, callback)
+		base_request(project_id, path, callback, nil, 'DELETE')
+	end
+	-- 
+	-- assign obj -> Database
+	--
+	setmetatable(obj, self)
+	self.__index = self
+	return obj
 end
---
--- Test -> PUT
---
-local function test_put()
-	local post_data = '{ "005" : {"session_id": "004", "text":"TEST_POST"}}'
-	local url = 'https://bool-8ba6d.firebaseio.com/users/000/sessions.json?auth=' .. AUTH_TOKEN
-	http.request(url, 'PUT', function(self,_, response)
-		print(response.response)
-	end, 
-	nil, post_data, nil)
-end
---
--- Test -> POST
---
-local function test_post()
-	local post_data = '{"session_id": "004", "text":"TEST_POST"}'
-	local url = 'https://bool-8ba6d.firebaseio.com/users/000/sessions.json?auth=' .. AUTH_TOKEN
-	http.request(url, 'POST', function(self,_, response)
-		print(response.response)
-	end, 
-	nil, post_data, nil)
-end
---
--- Test -> DELETE
---
-local function test_delete()
-	local url = 'https://bool-8ba6d.firebaseio.com/users/000/sessions.json'
-	http.request(url, 'DELETE', function(self,_, response)
-		print(response.response)
-	end, 
-	nil, nil, nil)
-end
+
+return Database
